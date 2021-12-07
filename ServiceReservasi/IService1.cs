8399b787fa4c5dccd3cbabcd17c7d329143556d3
Gala Pradipta <gala123pradipta@gmail.com>
2021-12-07 09:54:41 +0700

    Add project files.

---
+++ ServiceReservasi/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi); //method //proses input data
+        [OperationContract]
+        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        [OperationContract]
+        string deletePemesanan(string IDPemesanan);
+        [OperationContract]
+        List<CekLokasi> ReviewLokasi(); //menampilkan data yg ada di database (select all) //menampilkan isi dari yg ada di contract
+        [OperationContract]
+        List<DetailLokasi> DetailLokasi(); //menampilkan detail lokasi
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
+
+    }
+
+    [DataContract]
+    public class CekLokasi //daftar lokasi nongkrong
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variable dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeskripsiSingkat { get; set; }
+    }
+
+    [DataContract]
+    public class DetailLokasi //menampilkan detail lokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; } //variable dari public class
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeskripsiFull { get; set; }
+        [DataMember]
+        public int Kuota { get; set; }
+    }
+
+    [DataContract]
+    public class Pemesanan //create
+    {
+        [DataMember]
+        public string IDPemesanan { get; set; }
+        [DataMember]
+        public string NamaCustomer { get; set; }
+        [DataMember]
+        public string NoTelpon { get; set; }
+        [DataMember]
+        public int JumlahPemesanan { get; set; }
+        [DataMember]
+        public string IDLokasi { get; set; }
+    }
+ 
+
+}
+        
+   
---
+++ ServiceReservasi/Service1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace ServiceReservasi
+{
+    public class Service
+    {
+        string constring = "Data Source=ASUS-FOS9A28D;Initial Catalog=WCFReservasi;Persist Security Info=True;User ID=sa;Password=123";
+        SqlConnection connection;
+        SqlCommand com;
+    }
+
+    public List <DetailLokasi> DetailLokasi()
+    {
+        List <DetailLokasi> LokasiFull = new List<DetailLokasi>();
+        try
+        {
+            string sql = "select ID_lokasi, Nama_lokasi, Deskripsi_full, Kuota from dbo.Lokasi";
+            connection = new SqlConnection(constring);
+            com = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                DetailLokasi data = new DetailLokasi();
+
+                data.IDLokasi = reader.GetString(0);
+                data.NamaLokasi = reader.GetString(1);
+                data.DeskripsiFull = reader.GetString(2);
+                data.Kuota = reader.GetInt32(3);
+                LokasiFull.Add(data); //mengumpulkan data dari array
+            }
+            connection.Close(); //untuk menutup akses ke database   
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        return LokasiFull;
+    }
+
+    public string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi)
+    {
+        string a = "gagal";
+        try
+        {
+            string sql = "insert into dbo.Pemesanan values ('" + IDPemesanan + "', '" + NamaCustomer + "', '" + NoTelpon + "', " + JumlahPemesanan + ", '" + IDLokasi + "')";
+            connection = new SqlConnection(constring);
+            com = new SqlCommand(sql, connection);
+            connection.Open();
+            com.ExecuteNonQuery();
+            connection.Close();
+            a = "sukses";
+        }
+        catch (Exception es)
+        {
+            Console.WriteLine (es);
+        }
+        return a;
+    }
+}
